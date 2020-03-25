bcb30a6547055b1fc01b84ae80952bd2c3052908
Marc Lechuga <marc.lechuga@inserm.fr>
2020-03-25 13:19:57 +0100

    essai du script pur ajouter les attachements automatiquement

--- app/models/clone_attachment.rb
+++ app/models/clone_attachment.rb
@@ -1,2 +1,7 @@
 class CloneAttachment < ActiveRecord::Base
+ mount_uploader :attachment, AttachmentUploader
+ belongs_to :clone
+ #La ligne suivante permet de supprimer l'enregistrement ce Clone_attachment lorsqu'un fichier est supprimé
+ #dans le formulaire. En son absence, seul le fichier est supprimé et la valeur d'":attachement" effacée.
+ after_save { |clone_attachment| clone_attachment.destroy if clone_attachment.attachment.blank? }
 end
---
+++ lib/csv2attachment.rb
@@ -0,0 +1,11 @@
+require 'csv'
+	
+data = CSV.read('uploads2/clone-attachments-2020-03-24B.csv', headers:true, col_sep:";")
+data.each do |row|
+	parent = Clone.find(row['Clone_id'])
+	file = Dir['uploads2/clone_attachment/attachment/'+row['Id']+'/*']
+	file_path = file[0]
+	clone_attachment = CloneAttachment.new(attachment: Pathname.new(file_path).open)
+	parent.clone_attachments << clone_attachment
+	parent.save!
+end
